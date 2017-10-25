dbee5dcb5fee1c4d9f6f9d35ed7780eb92e3a578
JamesRiall <jamesrariall@gmail.com>
2017-10-25 08:30:40 +0100

    setting up dependencies

---
+++ src/containers/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../style/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Review App</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
